2583c546b9075f3647555f965269a5de48a40b05
Huy Sinh <huysinh@SinhHuy.local>
2022-10-19 16:02:27 +0700

    fix ebs & jenkins

--- modules/ebs/data.tf
+++
@@ -1,8 +0,0 @@
-# data "terraform_remote_state" "eks" {
-#   backend = "s3"
-#   config = {
-#     bucket = "terraform-on-aws-eks"
-#     key    = "dev/eks-cluster/terraform.tfstate"
-#     region = var.aws_region
-#   }
-# }
--- modules/ebs/iam_ebs_role.tf
+++ modules/ebs/iam_ebs_role.tf
@@ -41,3 +41,39 @@ resource "aws_iam_role_policy_attachment" "ebs_csi_iam_role_policy_attach" {
   #role       = var.worker_node_iam_role
   role = aws_iam_role.ebs_csi_iam_role.name
 }
+
+######################
+resource "aws_iam_policy" "eks_worknode_ebs_policy" {
+  name = "${var.name}-Amazon_EBS_CSI_Driver"
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "ec2:AttachVolume",
+        "ec2:CreateSnapshot",
+        "ec2:CreateTags",
+        "ec2:CreateVolume",
+        "ec2:DeleteSnapshot",
+        "ec2:DeleteTags",
+        "ec2:DeleteVolume",
+        "ec2:DescribeInstances",
+        "ec2:DescribeSnapshots",
+        "ec2:DescribeTags",
+        "ec2:DescribeVolumes",
+        "ec2:DetachVolume"
+      ],
+      "Resource": "*"
+    }
+  ]
+}
+POLICY
+}
+# And attach the new policy
+resource "aws_iam_role_policy_attachment" "worknode-AmazonEBSCSIDriver" {
+  policy_arn = aws_iam_policy.eks_worknode_ebs_policy.arn
+  role       = var.worker_node_iam_role
+}
